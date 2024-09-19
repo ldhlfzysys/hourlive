012542e58d7b2e980dc0941427071d64603e4889
donghuan1 <ldhlfzysys@163.com>
2024-09-19 16:17:31 +0800

    fix: 修改基础配置

--- apps/web-antd/src/preferences.ts
+++ apps/web-antd/src/preferences.ts
@@ -7,6 +7,24 @@ import { defineOverridesPreferences } from '@vben/preferences';
 export const overridesPreferences = defineOverridesPreferences({
   // overrides
   app: {
+    authPageLayout: 'panel-center',
+    enableCheckUpdates: false,
     name: import.meta.env.VITE_APP_TITLE,
+  },
+  breadcrumb: {
+    showHome: true,
+  },
+  copyright: {
+    companyName: 'HourLive',
+    companySiteLink: '',
+  },
+  sidebar: {
+    width: 170,
+  },
+  theme: {
+    builtinType: 'gray',
+    colorPrimary: 'hsl(240 5.9% 10%)',
+    mode: 'light',
+    radius: '0.75',
   },
 });
