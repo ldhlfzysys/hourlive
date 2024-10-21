da279fe15220114784063b7df679125be3abe7d1
liweixi <1248582385@qq.com>
2024-10-21 17:37:02 +0800

    路由改造拆解

--- apps/web-antd/src/router/routes/config/viewMap.ts
+++ apps/web-antd/src/router/routes/config/viewMap.ts
@@ -1,6 +1,36 @@
 import { $t } from '#/locales';
 
 const viewMap = {
+  content: {
+    component: () => import('#/views/customer/content.vue'),
+    meta: {
+      affixTab: true,
+      icon: 'lucide:tv-minimal-play',
+      title: $t('content'),
+    },
+    name: 'Content',
+    path: '/content',
+  },
+  liveaccount: {
+    component: () => import('#/views/customer/liveaccount.vue'),
+    meta: {
+      affixTab: true,
+      icon: 'lucide:user-round',
+      title: $t('liveaccount'),
+    },
+    name: 'CustomerLiveAccount',
+    path: '/liveaccount',
+  },
+  orders: {
+    component: () => import('#/views/customer/orders.vue'),
+    meta: {
+      affixTab: true,
+      icon: 'lucide:calendar-days',
+      title: $t('sample'),
+    },
+    name: 'Orders',
+    path: '/orders',
+  },
   sales: {
     component: () => import('#/views/agency/sales.vue'),
     meta: {
@@ -21,6 +51,16 @@ const viewMap = {
     name: 'Sample',
     path: '/sample',
   },
+  sampleshipping: {
+    component: () => import('#/views/customer/sampleshipping.vue'),
+    meta: {
+      affixTab: true,
+      icon: 'lucide:truck',
+      title: $t('sampleshipping'),
+    },
+    name: 'Sampleshipping',
+    path: '/sampleshipping',
+  },
   schedule: {
     component: () => import('#/views/common/schedule.vue'),
     meta: {
@@ -30,6 +70,16 @@ const viewMap = {
     },
     name: 'Schedule',
     path: '/schedule',
+  },
+  shippings: {
+    component: () => import('#/views/customer/shippings.vue'),
+    meta: {
+      affixTab: true,
+      icon: 'lucide:orders',
+      title: $t('mysampleshipping'),
+    },
+    name: 'Shippings',
+    path: '/shippings',
   },
 };
 
