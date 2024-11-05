41a93663f13d268363b64db3ce3de50e434ffc05
donghuan1 <ldhlfzysys@163.com>
2024-11-05 16:14:54 +0800

    添加content

--- apps/web-antd/src/store/content.ts
+++ apps/web-antd/src/store/content.ts
@@ -1,50 +1,178 @@
+// base
+import { computed, ref } from 'vue';
+
+import { notification } from 'ant-design-vue';
 import { defineStore } from 'pinia';
 
 import { requestClient } from '#/api/request';
+import { $t } from '#/locales';
 
-// Model & Query
-import type { BaseQuery, Content, IdQuery, StanderResult } from '#/types';
+// types
+import type { AddSample, Content, ContentQuery, StanderResult } from '#/types';
 
-// API
-enum ContentApi {
-  AllContent = 'allcontent',
-  CreateContent = 'createcontent',
-  DeleteContent = 'deletecontent',
-  UpdateContent = 'updatecontent',
+function _getAllContent(params?: ContentQuery) {
+  return requestClient.post<StanderResult<Content[]>>('content/query', params);
 }
 
-function getAllContent(params: BaseQuery) {
-  return requestClient.post<StanderResult<Content[]>>(
-    ContentApi.AllContent,
-    params,
-  );
+function _newContent(params: Content) {
+  return requestClient.post<StanderResult<Content>>('content/create', params);
 }
 
-function createContent(params: Content) {
+function _updateContent(params: Content) {
+  return requestClient.post<StanderResult<Content>>('content/update', params);
+}
+
+function _addSamples(params: AddSample) {
   return requestClient.post<StanderResult<Content>>(
-    ContentApi.CreateContent,
+    'content/addsample',
     params,
   );
 }
 
-function updateContent(params: Content) {
-  return requestClient.post<StanderResult<Content>>(
-    ContentApi.UpdateContent,
-    params,
-  );
-}
-
-function deleteContent(params: IdQuery) {
-  return requestClient.post<StanderResult<Content>>(
-    ContentApi.DeleteContent,
-    params,
-  );
-}
-
+// store
 export const useContentStore = defineStore('content-store', () => {
-  function $reset() {}
+  // loading
+
+  const contentLoading = ref(false);
+  const contentCreateLoading = ref(false);
+  const contentCreate = ref<Content>({
+    // 根据 Content 类型添加其他必要的初始字段
+  });
+  const addSample = ref<AddSample>({
+    content_id: -1,
+    sample_ids: [],
+  });
+  // contents
+  const contents = ref<Map<number, Content>>(new Map());
+
+  const contentList = computed(() => {
+    return [...contents.value.entries()]
+      .sort(([keyA], [keyB]) => keyB - keyA) // 按key从大到小排序
+      .map(([_, content]) => content); // 转换为Content的list
+  });
+
+  const showModal = ref(false);
+  const showAddSamplesModal = ref(false);
+
+  function makeCreate() {
+    showModal.value = true;
+    contentCreate.value = {
+      // 根据 Content 类型添加其他必要的初始字段
+    };
+  }
+  function makeUpdate(id: number) {
+    showModal.value = true;
+    const sample = contents.value.get(id);
+    if (sample) {
+      contentCreate.value = sample;
+    }
+  }
+
+  // query
+  const contentQuery = ref<ContentQuery>({
+    q_id: -1,
+    q_order: 'desc',
+    q_size: 30,
+  });
+
+  function $reset() {
+    contentLoading.value = false;
+    contentCreateLoading.value = false;
+    contentQuery.value.q_id = -1;
+
+    contentQuery.value = {
+      q_id: -1,
+      q_order: 'desc',
+      q_size: 5,
+    };
+    contents.value = new Map();
+  }
+
+  // methods
+  async function queryContent() {
+    try {
+      contentLoading.value = true;
+      const res = await _getAllContent(contentQuery.value);
+      if (res.success) {
+        if (res.data.length > 0) {
+          const lastContent = res.data.at(-1);
+          if (lastContent && lastContent.id) {
+            contentQuery.value.q_id = lastContent.id;
+          }
+        }
+        res.data.forEach((content) => {
+          if (content.id) {
+            contents.value.set(content.id, content);
+          }
+        });
+      }
+    } finally {
+      contentLoading.value = false;
+    }
+  }
+
+  async function createContent() {
+    try {
+      contentCreateLoading.value = true;
+      const res = await _newContent(contentCreate.value);
+      if (res.success && res.data.id) {
+        showModal.value = false;
+        contents.value.set(res.data.id, res.data);
+      } else {
+        notification.error({
+          description: res.message,
+          message: $t('addfail'),
+        });
+      }
+    } finally {
+      contentCreateLoading.value = false;
+    }
+  }
+
+  async function updateContent() {
+    try {
+      contentCreateLoading.value = true;
+      const res = await _updateContent(contentCreate.value);
+      if (res.success && res.data.id) {
+        showModal.value = false;
+        contents.value.set(res.data.id, res.data);
+      } else {
+        notification.error({
+          description: res.message,
+          message: $t('updatefail'),
+        });
+      }
+    } finally {
+      contentCreateLoading.value = false;
+    }
+  }
+
+  async function addSamples() {
+    try {
+      const res = await _addSamples(addSample.value);
+      if (res.success) {
+        showAddSamplesModal.value = false;
+      }
+    } finally {
+      showAddSamplesModal.value = false;
+    }
+  }
 
   return {
     $reset,
+    addSamples,
+    contentCreate,
+    contentCreateLoading,
+    contentList,
+    contentLoading,
+    contentQuery,
+    contents,
+    createContent,
+    makeCreate,
+    makeUpdate,
+    queryContent,
+    showAddSamplesModal,
+    showModal,
+    updateContent,
   };
 });
--- apps/web-antd/src/types/IContent.ts
+++ apps/web-antd/src/types/IContent.ts
@@ -1,7 +1,10 @@
 import type { LiveAccount } from './ILiveAccount';
 import type { Sample } from './ISample';
+import type { BaseQuery } from './ISystem';
 import type { TimeslotOrder } from './ITimeslotOrder';
 import type { Customer } from './IUser';
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface ContentQuery extends BaseQuery {}
 
 export interface AddSample {
   content_id: number;
@@ -9,17 +12,17 @@ export interface AddSample {
 }
 
 export interface Content {
-  id: number;
-  create_time: string; // DateTime in ISO format
-  update_time: string; // DateTime in ISO format
-  customer_id: number;
-  customer: Customer;
-  is_feiyue: string;
+  id?: number;
+  create_time?: string; // DateTime in ISO format
+  update_time?: string; // DateTime in ISO format
+  customer_id?: number;
+  customer?: Customer;
+  is_feiyue?: string;
   liveaccount_id?: number;
   liveaccount?: LiveAccount;
   content_link?: string;
   content_text?: string;
   content_desc?: string;
-  samples: Sample[];
-  timeslotorders: TimeslotOrder[];
+  samples?: Sample[];
+  timeslotorders?: TimeslotOrder[];
 }
