691cdbafebb2780fbe2dafe1aca43b3b17e9fc2d
donghuan1 <ldhlfzysys@163.com>
2024-11-09 14:37:26 +0800

    agency router

--- apps/web-antd/src/views/agency/home.vue
+++ apps/web-antd/src/views/agency/home.vue
@@ -2,7 +2,7 @@
 import { $t } from '@vben/locales';
 
 import { Card } from 'ant-design-vue';
-import { Package, Truck, TvMinimalPlay, UserRound } from 'lucide-vue-next';
+import { Eye, Podcast, Truck, Users, Video } from 'lucide-vue-next';
 
 import HourLivePage from '#/views/template/common.vue';
 
@@ -11,112 +11,21 @@ import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
 
 <template>
   <HourLivePage :content-overflow="true">
-    <template #header> </template>
+    <template #header></template>
 
     <template #content>
       <div class="flex flex-col p-3">
         <div class="flex flex-row gap-4">
-          <!-- Timeline -->
           <Card :title="$t('guide')">
             <div>
-              <!-- Item -->
+              <!-- 团队管理 -->
               <div class="flex gap-x-3">
-                <!-- Left Content -->
-                <div class="w-20 text-end">
-                  <span class="text-xs text-gray-500 dark:text-neutral-400">{{
-                    $t('addsample')
-                  }}</span>
-                </div>
-                <!-- End Left Content -->
-
-                <!-- Icon -->
-                <div
-                  class="relative after:absolute after:bottom-0 after:start-3.5 after:top-7 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 last:after:hidden dark:after:bg-neutral-700"
-                >
-                  <div
-                    class="relative z-10 flex size-7 items-center justify-center"
-                  >
-                    <div
-                      class="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"
-                    ></div>
-                  </div>
-                </div>
-                <!-- End Icon -->
-
-                <!-- Right Content -->
-                <div class="grow pb-8 pt-0.5">
-                  <h3
-                    class="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white"
-                  >
-                    <Package />
-                    <RouterLink to="/sample">
-                      {{ $t('createsample') }}
-                    </RouterLink>
-                  </h3>
-                  <p class="mt-1 text-sm text-gray-600 dark:text-neutral-400">
-                    {{ $t('addsampledesc') }}
-                  </p>
-                </div>
-                <!-- End Right Content -->
-              </div>
-              <!-- End Item -->
-
-              <!-- Item -->
-              <div class="flex gap-x-3">
-                <!-- Left Content -->
-                <div class="w-20 text-end">
-                  <span class="text-xs text-gray-500 dark:text-neutral-400">{{
-                    $t('createliveaccount')
-                  }}</span>
-                </div>
-                <!-- End Left Content -->
-
-                <!-- Icon -->
-                <div
-                  class="relative after:absolute after:bottom-0 after:start-3.5 after:top-7 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 last:after:hidden dark:after:bg-neutral-700"
-                >
-                  <div
-                    class="relative z-10 flex size-7 items-center justify-center"
-                  >
-                    <div
-                      class="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"
-                    ></div>
-                  </div>
-                </div>
-                <!-- End Icon -->
-
-                <!-- Right Content -->
-                <div class="grow pb-8 pt-0.5">
-                  <h3
-                    class="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white"
-                  >
-                    <UserRound />
-                    <RouterLink to="/liveaccount">
-                      {{ $t('createliveaccount') }}
-                    </RouterLink>
-                  </h3>
-                  <button
-                    class="-ms-1 mt-1 inline-flex items-center gap-x-2 rounded-lg border border-transparent p-1 text-xs text-gray-500 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                    type="button"
-                  >
-                    {{ $t('createliveaccountdesc') }}
-                  </button>
-                </div>
-                <!-- End Right Content -->
-              </div>
-              <!-- End Item -->
-
-              <!-- Item -->
-              <div class="flex gap-x-3">
-                <!-- Left Content -->
                 <div class="w-20 text-end">
                   <span class="text-xs text-gray-500 dark:text-neutral-400">
-                    {{ $t('createcontent') }}
+                    {{ $t('teammanagement') }}
                   </span>
                 </div>
-                <!-- End Left Content -->
 
-                <!-- Icon -->
                 <div
                   class="relative after:absolute after:bottom-0 after:start-3.5 after:top-7 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 last:after:hidden dark:after:bg-neutral-700"
                 >
@@ -128,37 +37,30 @@ import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
                     ></div>
                   </div>
                 </div>
-                <!-- End Icon -->
 
-                <!-- Right Content -->
                 <div class="grow pb-8 pt-0.5">
                   <h3
                     class="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white"
                   >
-                    <TvMinimalPlay />
-                    <RouterLink to="/content">
-                      {{ $t('createcontent') }}
+                    <Users />
+                    <RouterLink to="/streamer">
+                      {{ $t('teammanagement') }}
                     </RouterLink>
                   </h3>
                   <p class="mt-1 text-sm text-gray-600 dark:text-neutral-400">
-                    {{ $t('createcontentdesc') }}
+                    {{ $t('teammanagementdesc') }}
                   </p>
                 </div>
-                <!-- End Right Content -->
               </div>
-              <!-- End Item -->
 
-              <!-- Item -->
+              <!-- 房间管理 -->
               <div class="flex gap-x-3">
-                <!-- Left Content -->
                 <div class="w-20 text-end">
-                  <span class="text-xs text-gray-500 dark:text-neutral-400">{{
-                    $t('shippingsample')
-                  }}</span>
+                  <span class="text-xs text-gray-500 dark:text-neutral-400">
+                    {{ $t('room') }}
+                  </span>
                 </div>
-                <!-- End Left Content -->
 
-                <!-- Icon -->
                 <div
                   class="relative after:absolute after:bottom-0 after:start-3.5 after:top-7 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 last:after:hidden dark:after:bg-neutral-700"
                 >
@@ -170,25 +72,126 @@ import 'vue-virtual-scroller/dist/vue-virtual-scroller.css';
                     ></div>
                   </div>
                 </div>
-                <!-- End Icon -->
 
-                <!-- Right Content -->
+                <div class="grow pb-8 pt-0.5">
+                  <h3
+                    class="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white"
+                  >
+                    <Video />
+                    <RouterLink to="/room">
+                      {{ $t('room') }}
+                    </RouterLink>
+                  </h3>
+                  <p class="mt-1 text-sm text-gray-600 dark:text-neutral-400">
+                    {{ $t('roomdesc') }}
+                  </p>
+                </div>
+              </div>
+
+              <!-- 小时直播包 -->
+              <div class="flex gap-x-3">
+                <div class="w-20 text-end">
+                  <span class="text-xs text-gray-500 dark:text-neutral-400">
+                    {{ $t('hourlivepackage') }}
+                  </span>
+                </div>
+
+                <div
+                  class="relative after:absolute after:bottom-0 after:start-3.5 after:top-7 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 last:after:hidden dark:after:bg-neutral-700"
+                >
+                  <div
+                    class="relative z-10 flex size-7 items-center justify-center"
+                  >
+                    <div
+                      class="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"
+                    ></div>
+                  </div>
+                </div>
+
+                <div class="grow pb-8 pt-0.5">
+                  <h3
+                    class="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white"
+                  >
+                    <Podcast />
+                    <RouterLink to="/sales">
+                      {{ $t('hourlivepackage') }}
+                    </RouterLink>
+                  </h3>
+                  <p class="mt-1 text-sm text-gray-600 dark:text-neutral-400">
+                    {{ $t('hourlivepackagedesc') }}
+                  </p>
+                </div>
+              </div>
+
+              <!-- 收货地址 -->
+              <div class="flex gap-x-3">
+                <div class="w-20 text-end">
+                  <span class="text-xs text-gray-500 dark:text-neutral-400">
+                    {{ $t('shipping_address') }}
+                  </span>
+                </div>
+
+                <div
+                  class="relative after:absolute after:bottom-0 after:start-3.5 after:top-7 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 last:after:hidden dark:after:bg-neutral-700"
+                >
+                  <div
+                    class="relative z-10 flex size-7 items-center justify-center"
+                  >
+                    <div
+                      class="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"
+                    ></div>
+                  </div>
+                </div>
+
                 <div class="grow pb-8 pt-0.5">
                   <h3
                     class="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white"
                   >
                     <Truck />
-                    <RouterLink to="/content">
-                      {{ $t('shippingsample') }}
+                    <RouterLink to="/shippingaddress">
+                      {{ $t('shipping_address') }}
                     </RouterLink>
                   </h3>
                   <p class="mt-1 text-sm text-gray-600 dark:text-neutral-400">
-                    {{ $t('shippingsampledesc') }}
+                    {{ $t('shipping_address_desc') }}
                   </p>
                 </div>
-                <!-- End Right Content -->
               </div>
-              <!-- End Item -->
+
+              <!-- 样品管理 -->
+              <div class="flex gap-x-3">
+                <div class="w-20 text-end">
+                  <span class="text-xs text-gray-500 dark:text-neutral-400">
+                    {{ $t('store') }}
+                  </span>
+                </div>
+
+                <div
+                  class="relative after:absolute after:bottom-0 after:start-3.5 after:top-7 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 last:after:hidden dark:after:bg-neutral-700"
+                >
+                  <div
+                    class="relative z-10 flex size-7 items-center justify-center"
+                  >
+                    <div
+                      class="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"
+                    ></div>
+                  </div>
+                </div>
+
+                <div class="grow pb-8 pt-0.5">
+                  <h3
+                    class="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white"
+                  >
+                    <Eye />
+                    <RouterLink to="/agency/sample">
+                      {{ $t('store') }}
+                    </RouterLink>
+                  </h3>
+                  <p class="mt-1 text-sm text-gray-600 dark:text-neutral-400">
+                    {{ $t('sampledesc') }}
+                  </p>
+                </div>
+              </div>
             </div>
           </Card>
           <Card title="今日场次">
