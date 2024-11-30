a17fe126bf7c61e8b8a1bafe6aee4c6703e78cb3
liweixi <1248582385@qq.com>
2024-11-30 10:30:34 +0800

    订单详情UI优化

--- apps/web-antd/src/components/orderApendModal.vue
+++ apps/web-antd/src/components/orderApendModal.vue
@@ -1,60 +1,46 @@
-<!-- <script lang="ts" setup>
-import type { SlotEvent } from '#/types';
-
-import { computed, ref } from 'vue';
-import { RecycleScroller } from 'vue-virtual-scroller';
+<script lang="ts" setup>
+import { computed } from 'vue';
 
 import { AccessControl } from '@vben/access';
-import { $t } from '@vben/locales';
 
-import { useElementBounding } from '@vueuse/core';
-import { Button, Descriptions, DescriptionsItem, Modal } from 'ant-design-vue';
+import { Button, Modal } from 'ant-design-vue';
 
-import Empty from '#/components/empty.vue';
-import SampleCard from '#/components/samplecard.vue';
-import { useAgencyStore, useSampleStore, useTimeslotOrderStore } from '#/store';
+import { useTimeslotOrderStore } from '#/store';
 
 defineOptions({
   name: 'OrderDetailModal',
 });
 
-const props = defineProps<{
-
-}>();
+const props = defineProps<{}>();
 const orderStore = useTimeslotOrderStore();
 
 const maxHeight = computed(() => {
   return window.innerHeight * 0.7;
 });
 
+// Define a method to handle the button click
+const handleConfirmAppend = () => {
+  // Add your logic here
+  console.log('Confirm append clicked');
+};
 </script>
 
 <template>
   <Modal
     v-model:open="orderStore.showApendModal"
     :body-style="{ overflowY: 'auto', maxHeight: `${maxHeight}px` }"
-    title="追加订单"
     style="top: 10px; width: 60%"
+    title="追加订单"
     @cancel="orderStore.showApendModal = false"
   >
-    <div class="flex h-full flex-1 flex-col">
-      
-    </div>
+    <div class="flex h-full flex-1 flex-col"></div>
 
     <template #footer>
-
       <AccessControl :codes="['super']">
-        <Button
-          key="submit"
-          type="primary"
-          @click=""
-        >
+        <Button key="submit" type="primary" @click="handleConfirmAppend">
           确认追加
-
         </Button>
-       
-
       </AccessControl>
     </template>
   </Modal>
-</template> -->
+</template>
